d3f48e3acdb38c91c8bb17ce61589599cfeac08f
Kai Lemke <kai.lemke91@gmx.de>
2016-01-03 01:25:16 +0100

    makefull & makediff are tto much $MHOME was partly masked

--- backup.sh
+++ backup.sh
@@ -25,6 +25,14 @@ DAY=`date +%u`
 DATE=$YEAR-$WEEK-$DAY		# Date: Year - calendar week - day (ISO = Monday first)
 BACKUPDIR=/opt/backup/$USER
 BASE=$BACKUPDIR/$HOST-$USER
+
+# users home directory
+BKPFULL=$BASE-1=full-$YEAR
+CATFULL=$BASE-1=full_cat-$YEAR
+BKPDIFF=$BASE-2=diff-$YEAR-$WEEK
+CATDIFF=$BASE-2=diff_cat-$YEAR-$WEEK
+BKPINC=$BASE-3=inc-$DATE
+CATINC=$BASE-3=inc_cat-$DATE
 BHOME="
          -R $HOME 
          -zbzip2 
@@ -46,32 +54,17 @@ warn() {
 	notify-send -u critical "$msg" &
 }		# Make shure, the system will not be shut down.
 
-testman() {
-	if [ ! -f $BASE.dmd ];then
-		dar_manager -C $BASE.dmd
-	fi;
-}
-
 testdir() {
 	if [ ! -d $1 ];then
 		mkdir $1
 	fi;
 }
 
-bfull() {
-	dar -c $BASE-1=full-$YEAR -@ $BASE-1=full_cat-$YEAR $BHOME &&
-		$MHOME -A $BASE-1=full_cat-$YEAR $BASE-1=full-$YEAR
-}		# full backup of home directory and add to database
-
-bdiff() {
-	dar -A $BASE-1=full_cat-$YEAR -c $BASE-2=diff-$YEAR-$WEEK -@ $BASE-2=diff_cat-$YEAR-$WEEK $BHOME &&
-		$MHOME -A $BASE-2=diff_cat-$YEAR-$WEEK $BASE-2=diff-$YEAR-$WEEK
-}		# differential backup of home directory and add to database
-
-binc() {
-	dar -A $BASE-2=diff_cat-$YEAR-$WEEK -c $BASE-3=inc-$DATE -@ $BASE-3=inc_cat-$DATE $BHOME &&
-		$MHOME -A $BASE-3=inc_cat-$DATE $BASE-3=inc-$DATE
-}		# incremental backup of home directory and add to database
+testman() {
+	if [ ! -f $BASE.dmd ];then
+		dar_manager -C $BASE.dmd
+	fi;
+}
 
 testb() {
 	if [ ! -f $1 ];then
@@ -84,6 +77,21 @@ testb() {
 		echo "$2 backup is already done."
 	fi;
 }
+
+fullhome() {
+	dar -c $BKPFULL -@ $CATFULL $BHOME &&
+		$MHOME -A $CATFULL $BKPFULL
+}		# full backup of home directory and add to database
+
+diffhome() {
+	dar -A $CATFULL -c $BKPDIFF -@ $CATDIFF $BHOME &&
+		$MHOME -A $CATDIFF $BKPDIFF
+}		# differential backup of home directory and add to database
+
+inchome() {
+	dar -A $CATDIFF -c $BKPINC -@ $CATINC $BHOME &&
+		$MHOME -A $CATINC $BKPINC
+}		# incremental backup of home directory and add to database
 
 # 03. body
 # --------
@@ -104,7 +112,7 @@ pacman -Qs > $HOME/log/paclist
 {
 	date +%Y-%m-%d@%T
 	echo "Running backup."
-	testb "$BASE-1=full_cat-$YEAR.1.dar" Full bfull
-	testb "$BASE-2=diff_cat-$YEAR-$WEEK.1.dar" Differential bdiff
-	testb "$BASE-3=inc_cat-$DATE.1.dar" Incremental binc
+	testb "$CATFULL.1.dar" Full fullhome
+	testb "$CATDIFF.1.dar" Differential diffhome
+	testb "$CATINC.1.dar" Incremental inchome
 } > $HOME/log/backup.log
